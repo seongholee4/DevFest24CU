73f1e6b008abb9a6a71cd33546b1c1bcc5fb6174
Seongho Lee <seongholee4@gmail.com>
2024-02-03 21:40:04 -0500

    Added members API endpoints

--- server/server.js
+++ server/server.js
@@ -27,92 +27,77 @@ async function main() {
     }
 }
 
-//Fetching of data from database
-app.get("/api/getMember", async (req, res) => {
-    const data = (req?.body);
-    console.log(data);
-    const result = await db.collection("members").find().toArray();
-    res.json(result);
-});
+/* Member */
 
+app.get("/api/addMember", async (req, res) => {
+    let data = req?.body;
 
-app.post("/api/addMember", async (req, res) => {
-    console.log("addMember req received");
-
-    //Finds the note in body of req and adds it to the database
-    const data = req?.body;
+    if (!data.hasOwnProperty("districtNum")) {
+        console.error("ERROR: No district number received");
+        res.send(null);
+    } else if (!data.hasOwnProperty("name")) {
+        data.name = null;
+    } else if (!data.hasOwnProperty("borough")) {
+        data.borough = null;
+    } else if (!data.hasOwnProperty("party")) {
+        data.party = null;
+    } else if (!data.hasOwnProperty("districts")) {
+        data.districts = null;
+    } else if (!data.hasOwnProperty("email")) {
+        data.email = null;
+    } else if (!data.hasOwnProperty("zipCodes")) {
+        data.zipCodes = null;
+    } else if (!data.hasOwnProperty("imageURL")) {
+        data.imageURL = null;
+    }
     const result = await db.collection("members").insertOne(data);
     res.send(result);
+});
+
+app.get("/api/getMember", async (req, res) => {
+    let zipCode = (req?.body).zipCode;
+    zipCode = parseInt(zipCode);
+    const result = await db.collection("members").find({ zipCodes: { $in: [zipCode]}}).toArray();
+    if (result.length == 0) {
+        console.error(`ERROR: No council members found for zip code ${zipCode}`);
+        res.send(null);
+    } else {
+        res.json(result[0]);
+    }
 });
 
 app.post("/api/deleteMember", async (req, res) => {
     console.log("deleteMember received");
 
     //Finds the note in body of req and deletes it from the database
-    const deleteID = (req?.body).deleteMemberID;
-    const result = await db.collection("members").deleteOne({_id: new ObjectId(deleteID)});
+    const districtNum = (req?.body).districtNum;
+    const result = await db.collection("members").deleteOne({districtNum: districtNum});
 
     //Error check if the deletion didn't work
     if (result.deletedCount == 0) {
-        console.error(`ERROR: Member ${deleteID} was not deleted`);
+        console.error(`ERROR: Member ${districtNum} was not deleted`);
     }
     res.send(result);
 });
 
-// update
 app.post("/api/updateMember", async (req, res) => {
     console.log("updateMember received");
     const data = req?.body;
-    const id = data._id;
-    delete data._id;
-    const result = await db.collection("members").updateOne({_id: new ObjectId(id)}, {$set: data});
+    // check if the data is valid
+    if (!data) {
+        console.error("ERROR: No data received");
+        res.send(null);
+    } else if (!data.districtNum) {
+        console.error("ERROR: No district number received");
+        res.send(null);
+    }
+    const result = await db.collection("members").updateOne({districtNum: data.districtNum}, {$set: data});
     res.send(result);
 });
-
-//Called when a council member must be replaced
-app.post("/api/updateMember", async (req, res) => {
-    console.log("replaceMember req received");
-
-    //Finds the note in body of req and adds it to the database
-    //const data = req?.body;
-    const query={"districtNum": 8}
-    const data ={
-        districtNum:8,
-        name:"Diana Ayala",
-        borough: ["Manhattan", "Bronx"],
-        party:"Democrat",
-        districts:[
-            "Mott Haven-Port Morris", "Melrose", "Concourse-Concourse Village", "Upper East Side-Carnegie Hill", "Upper East Side-Yorkville", "East Harlem (South)", "East Harlem (North)", "Randall's Island"
-        ],
-        email: "District8@council.nyc.gov"};    
-    const result = await db.collection("members").replaceOne(query, data);
-    res.send(result);
-})
 
 
 /* Users */
 
-app.get("/api/getUser", async (req, res) => {
-    const data = await db.collection("users").find().toArray();
-    res.json(data);
-});
-
-
-//Fetch legislative members database
-app.get("/api/getMembers", async (req, res) => {
-    const data = await fetch(
-        `https://webapi.legistar.com/v1/nyc/Bodies`
-    );
-    const result = await data.json();
-
-
-    console.log("FETCHED!");
-    console.log(result);
-    res.json(data);
-})
-
-
-//Called when a user must be added
 app.post("/api/addUser", async (req, res) => {
     console.log("addUser req received");
 
@@ -120,44 +105,18 @@ app.post("/api/addUser", async (req, res) => {
     const data = req?.body;
     const result = await db.collection("users").insertOne(data);
     res.send(result);
-})
+});
 
-//Called when a council member must be added
-app.post("/api/addMember", async (req, res) => {
-    console.log("addMember req received");
-
-    //Finds the note in body of req and adds it to the database
-    //const data = req?.body;
-    const data ={
-        districtNum:12,
-        name:"Kevin C. Riley",
-        borough: ["Bronx"],
-        party:"Democrat",
-        districts:[
-            "Co-op City", "Pelham Gardens", "Allerton", "Williamsbridge-Olinville", "Eastchester-Edenwald-Baychester", "Wakefield-Woodlawn", "Pelham Bay Park"
-        ],
-        email: "District12@council.nyc.gov"};
-    const result = await db.collection("members").insertOne(data);
-    res.send(result);
-})
+app.get("/api/getUser", async (req, res) => {
+    const data = await db.collection("users").find().toArray();
+    res.json(data);
+});
 
 
+/* Laws */
 
-//Called when a note must be deleted
-app.post("/api/deleteNote", async (req, res) => {
-    console.log("deleteNote received");
 
-    //Finds the note in body of req and deletes it from the database
-    const deleteID = (req?.body).deleteNoteID;
-    const result = await db.collection("notes").deleteOne({_id: new ObjectId(deleteID)});
-
-    //Error check if the deletion didn't work
-    if (result.deletedCount == 0) {
-        console.error(`ERROR: Note ${deleteID} was not deleted`);
-    }
-    res.send(result);
-})
-
+/* Issues */
 
 
 main().catch(console.error);
